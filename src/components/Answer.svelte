ccb9836cf7dad73e12b88276a94d709fe6976997
pimmink <pimmink@icloud.com>
2021-01-10 22:49:32 +0100

    start

--- src/components/Answer.svelte
+++ src/components/Answer.svelte
@@ -1,7 +1,31 @@
 <script type="ts">
-  export let answerTitle: string;
+    export let answerTitle: string;
 </script>
 
 <div class="answer">
-  <h2>{answerTitle}</h2>
+    <h2 class="quote">{answerTitle}</h2>
+    <div class="options">
+        <div class="option"><span class="code">A</span> Kaas</div>
+        <div class="option answer"><span class="code">B</span> Worst</div>
+        <div class="option"><span class="code">C</span> Bier</div>
+    </div>
+    <div class="stats">
+        <div class="statgroup age">
+            <div class="agegroup" data-age="0-12">
+
+            </div>
+            <div class="agegroup" data-age="13-19"></div>
+            <div class="agegroup" data-age="20-29"></div>
+        </div>
+        <div class="statgroup state">
+            <div class="stategroup" data-state="ZH"></div>
+            <div class="stategroup" data-state="GL"></div>
+            <div class="stategroup" data-state="FL"></div>
+        </div>
+        <div class="statgroup gender">
+            <div class="gendergroup" data-gender="female"></div>
+            <div class="gendergroup" data-gender="male"></div>
+            <div class="gendergroup" data-gender="shemale"></div>
+        </div>
+    </div>
 </div>
