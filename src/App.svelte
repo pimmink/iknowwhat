ad189a6df52dd84b4e9353005bb310071cacba88
pimmink <pimmink@icloud.com>
2021-01-10 20:52:52 +0100

    update app

--- src/App.svelte
+++ src/App.svelte
@@ -1,26 +1,25 @@
 <script lang="ts">
-	import Answer from './components/Answer.svelte';
+    import Answer from './components/Answer.svelte';
 
-	export let name: string;
+    export let name: string;
 
-	type AnswerType = {
-		correctAnswer: string;
-		title: string;
-	}
+    type AnswerType = {
+        correctAnswer: string;
+        title: string;
+    }
 
-	const answers: Array<AnswerType> = [
-		{ correctAnswer: '4', title: 'Haha' },
-	];
+    const answers: Array<AnswerType> = [
+        {correctAnswer: '4', title: 'Haha'},
+    ];
 
 </script>
 
-
 <main>
-	{#each answers as answer}
-		<Answer answerTitle={answer.title} />
-	{/each}
+    {#each answers as answer}
+        <Answer answerTitle={answer.title}/>
+    {/each}
     <div>
-        <h1>I know what.</h1>
+        <h1>{ name }</h1>
     </div>
     <label for="episodes">Which episode:</label>
     <select name="episodes" id="episodes">
@@ -29,37 +28,57 @@
 </main>
 
 <style type="text/scss">
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap');
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-
-    $color: red;
-
-    h1 {
-        color: $color;
+  @mixin prefix($property, $value, $vendors: webkit moz ms o khtml, $default: true) {
+    @if $vendors {
+      @each $vendor in $vendors {
+          #{"-" + $vendor + "-" + $property}: #{$value};
+      }
     }
-
-    div {
-        background: green;
-
-    > p {
-        color: #fff;
+    @if $default {
+        #{$property}: #{$value};
     }
+  }
+
+  $grey: #5E5E5E;
+  $lightgrey: #F2F2F2;
+  $orange: #ff5218;
+
+  $gradientOrange: #ff793d, #f43e01;
+
+  $font: 'Poppins', "Helvetica Neue", Helvetica, Arial, sans-serif;
+  $fontBold: 600;
+  $fontNormal: 400;
+
+  :global(body) {
+    background-color: $lightgrey;
+    color: $grey;
+  }
+  main {
+    text-align: center;
+    padding: 0;
+    max-width: 240px;
+    margin: 0 auto;
+    font-family:  $font;
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    font-weight: $fontBold;
+  }
+
+  h1 {
+    color: $orange;
+    background: -webkit-linear-gradient($gradientOrange);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    font-size: 4em;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
     }
+  }
+
 </style>
